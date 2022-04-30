3e5bb563dfea6c61447eba9399d01f60017c6dd8
Michael Hide <mhide@thezebra.com>
2022-04-30 18:28:35 -0500

    Refactor Config to ELConfig.

--- src/main/java/com/hidethemonkey/elfim/ELPlugin.java
+++ src/main/java/com/hidethemonkey/elfim/ELPlugin.java
@@ -15,7 +15,7 @@
  *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
  * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
@@ -28,39 +28,36 @@ import com.hidethemonkey.elfim.listeners.PlayerEventListeners;
 import com.hidethemonkey.elfim.listeners.ServerEventListeners;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class ELFIMPlugin extends JavaPlugin {
-
-  private String pluginVersion;
+public class ELPlugin extends JavaPlugin {
 
   @Override
   public void onEnable() {
     saveDefaultConfig();
-    Config config = new Config(this.getConfig(), getLogger());
-    pluginVersion = this.getDescription().getVersion();
+    ELConfig elConfig = new ELConfig(this.getConfig(), getLogger());
     // Save the plugin version to use in MessageHandler
-    config.setVersion(pluginVersion);
+    elConfig.setVersion(this.getDescription().getVersion());
     saveConfig();
     // Store name on config for easy access later (not saved to file)
-    config.setPluginName(this.getName());
+    elConfig.setPluginName(this.getName());
 
     saveResource("advancements.yml", true);
     AdvancementConfig advConfig = new AdvancementConfig(getDataFolder());
 
-    if (checkToken(config.getToken()) && checkChannel(config.getChannelId())) {
+    if (checkToken(elConfig.getToken()) && checkChannel(elConfig.getChannelId())) {
       // Register Server Events
       getServer()
           .getPluginManager()
-          .registerEvents(new ServerEventListeners(config), this);
+          .registerEvents(new ServerEventListeners(elConfig), this);
 
       // Register Player Events
       getServer()
           .getPluginManager()
-          .registerEvents(new PlayerEventListeners(config, advConfig), this);
+          .registerEvents(new PlayerEventListeners(elConfig, advConfig), this);
     } else {
       getLogger().warning("The Slack API token or channel is not configured!");
     }
     // Register configuration commands
-    this.getCommand("elfs").setExecutor(new CommandELFS(config));
+    this.getCommand("elfs").setExecutor(new CommandELFS(elConfig));
   }
 
   @Override
@@ -69,7 +66,7 @@ public class ELFIMPlugin extends JavaPlugin {
   }
 
   private boolean checkChannel(String channelId) {
-    if (channelId.trim().isBlank() || channelId.trim().equals(Config.defaultSlackChannelId)) {
+    if (channelId.trim().isBlank() || channelId.trim().equals(ELConfig.defaultSlackChannelId)) {
       getLogger().severe("The Slack channel must be set in the plugin config.yml file!");
       return false;
     }
@@ -77,7 +74,7 @@ public class ELFIMPlugin extends JavaPlugin {
   }
 
   private boolean checkToken(String token) {
-    if (token.trim().isBlank() || token.trim().equals(Config.defaultSlackToken)) {
+    if (token.trim().isBlank() || token.trim().equals(ELConfig.defaultSlackToken)) {
       getLogger().severe("The Slack API token is not configured, please update it in the plugin config.yml file!");
       return false;
     }
